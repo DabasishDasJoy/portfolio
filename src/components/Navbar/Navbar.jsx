eb0c517a0bdcc8f7b76dc241d091d0a761d68094
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-12-10 02:51:54 +0600

    home page image added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="absolute flex flex-col gap-5 justify-center items-center border-2 right-5 top-0 bottom-0">
+    <div className="fixed flex flex-col gap-5 justify-center items-end right-5 top-0 bottom-0">
       <div className="nav-item flex items-center rounded-3xl">
-        <p className="nav-text overflow-hidden">About</p>
-        <div className="nav-icon rounded-[50%] p-2 bg-gray-300">
+        <p className="nav-text overflow-hidden">Home</p>
+        <Link to={"/"} className="nav-icon rounded-[50%] p-2 bg-gray-300">
           <FaHome></FaHome>
-        </div>
+        </Link>
+      </div>
+      <div className="nav-item flex items-center rounded-3xl">
+        <p className="nav-text overflow-hidden">Blog</p>
+        <Link className="nav-icon rounded-[50%] p-2 bg-gray-300">
+          <FaHome></FaHome>
+        </Link>
+      </div>
+      <div className="nav-item flex items-center rounded-3xl">
+        <p className="nav-text overflow-hidden">Portfolio</p>
+        <Link className="nav-icon rounded-[50%] p-2 bg-gray-300">
+          <FaHome></FaHome>
+        </Link>
+      </div>
+      <div className="nav-item flex items-center rounded-3xl">
+        <p className="nav-text overflow-hidden">Skills</p>
+        <Link className="nav-icon rounded-[50%] p-2 bg-gray-300">
+          <FaHome></FaHome>
+        </Link>
       </div>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { ImFacebook } from "react-icons/im";
+import { Link } from "react-router-dom";
+import myImg from "../../assets/1-01-removebg-preview.png";
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-primary">
-      <h1>Home</h1>
+      <div className="inline-block mt-32 relative">
+        <img src={myImg} alt="" className="z-10 relative" />
+
+        {/* Circle */}
+        <div className="border-r-[1px] -rotate-12 rounded-[50%] w-[500px] h-[500px] absolute top-20 left-0 border-b-[1px] border-gray-500"></div>
+
+        {/* Social Icons */}
+        <div className="z-50 rounded-[50%] w-[500px] h-[500px] absolute top-20 left-0">
+          <Link
+            to={"https://www.facebook.com/dabasishdas.joy/"}
+            className="cursor-pointer border rounded-[50%] p-3 absolute right-9 top-10 bg-primary"
+          >
+            <ImFacebook></ImFacebook>
+          </Link>
+          <Link
+            // to={"https://www.facebook.com/dabasishdas.joy/"}
+            className="cursor-pointer border rounded-[50%] p-3 absolute -right-3 top-[150px] bg-primary"
+          >
+            <ImFacebook></ImFacebook>
+          </Link>
+          <Link
+            // to={"https://www.facebook.com/dabasishdas.joy/"}
+            className="cursor-pointer border rounded-[50%] p-3 absolute -right-2 top-[250px] bg-primary"
+          >
+            <ImFacebook></ImFacebook>
+          </Link>
+        </div>
+
+        {/* Second circle */}
+        {/* <div className="border-r rounded-[50%] w-[300px] h-[300px] absolute top-20 left-28 -rotate-45 border-b"></div> */}
+      </div>
     </div>
   );
 };
