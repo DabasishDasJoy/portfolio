51e9e713418c3f036ca94521b9a5bbfc40bb316f
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-12-11 22:48:21 +0600

    refactor contact page

--- src/pages/Contact/Contact/Contact.jsx
+++ src/pages/Contact/Contact/Contact.jsx
@@ -40,27 +40,30 @@ const Contact = () => {
     <div className="bg-secondary min-h-screen px-20 py-10">
       <PageTitle subtitle={"Contact Me"}>Let's Discuss</PageTitle>
 
-      <section className="text-white flex items-center my-10">
-        <div className="flex flex-col gap-5 ">
+      <section className="text-white flex my-10">
+        <div className="flex flex-col gap-5 text-sm">
+          <h6 className="text-xl font-bold">Please Feel Free to Let Me Know</h6>
           <p className="">
             If you have any queries, please feel free to let me know.
           </p>
-          <div className="flex items-center gap-2">
-            <MdLocationOn className="w-6 h-6" />
-            <p>Chattogram, Bangladesh</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <MdCall className="w-6 h-6" />
-            <p>+880 1571 763498</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <GrMail className="w-6 h-6" />
-            <p>dabasish.joy9@gmail.com</p>
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center gap-2">
+              <MdLocationOn className="w-6 h-6" />
+              <p>Chattogram, Bangladesh</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <MdCall className="w-6 h-6" />
+              <p>+880 1571 763498</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <GrMail className="w-6 h-6" />
+              <p>dabasish.joy9@gmail.com</p>
+            </div>
           </div>
 
           <div className="flex gap-5">
             <a
-              href=""
+              href="https://www.facebook.com/dabasishdas.joy/"
               target={"_blank"}
               rel="noreferrer"
               className="border p-3 rounded-full"
@@ -68,7 +71,7 @@ const Contact = () => {
               <FaFacebookF />
             </a>
             <a
-              href=""
+              href="https://github.com/DabasishDasJoy"
               target={"_blank"}
               rel="noreferrer"
               className="border p-3 rounded-full"
@@ -76,7 +79,7 @@ const Contact = () => {
               <FiGithub />
             </a>
             <a
-              href=""
+              href="https://www.linkedin.com/in/dabasishjoy/"
               target={"_blank"}
               rel="noreferrer"
               className="border p-3 rounded-full"
@@ -90,14 +93,14 @@ const Contact = () => {
           onSubmit={sendEmail}
           className=" flex flex-col w-full px-10"
         >
-          <div className="grid grid-cols-6 gap-5 text-black">
+          <div className="grid grid-cols-6 gap-5 text-white">
             <div className="col-span-full sm:col-span-3">
               <input
                 id="Name"
                 name="user_name"
                 type="text"
                 placeholder="Name"
-                className="w-full rounded-full px-3 py-2"
+                className="w-full rounded-full px-3 py-2 bg-gray-700 placeholder:text-gray-300"
               />
             </div>
             <div className="col-span-full sm:col-span-3">
@@ -105,7 +108,7 @@ const Contact = () => {
                 type="email"
                 name="user_email"
                 placeholder="Your Email"
-                className="w-full rounded-full px-3 py-2"
+                className="w-full rounded-full px-3 py-2 bg-gray-700 placeholder:text-gray-300"
               />
             </div>
             <div className="col-span-full">
@@ -113,7 +116,7 @@ const Contact = () => {
                 type="text"
                 name="subject"
                 placeholder="Subject"
-                className="w-full rounded-full px-3 py-2"
+                className="w-full rounded-full px-3 py-2 bg-gray-700 placeholder:text-gray-300"
               />
             </div>
             <div className="col-span-full">
@@ -121,7 +124,7 @@ const Contact = () => {
                 id="message"
                 name="message"
                 placeholder="Your Message"
-                className="w-full h-[200px] rounded-xl px-3 py-2"
+                className="w-full h-[200px] rounded-xl px-3 py-2 bg-gray-700 placeholder:text-gray-300"
               ></textarea>
             </div>
           </div>
